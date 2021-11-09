464de1aaa63fad0ce59626f0fd6eafc220133d16
courtney long <courtneylong877@gmail.com>
2021-11-09 14:19:01 -0500

    Added routes and the schema

--- models/workout.js
+++ models/workout.js
@@ -3,14 +3,34 @@ const mongoose = require ("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema ({
+    type: {
+        type: String,
+        required: "Enter the type of your workout."
+    },
     name: {
         type: String,
         trim: true,
         required: "Enter the name of your workout."
     },
-    value: {
+    duration: {
         type: Number,
-        required: "Enter the weight/duration of your workout."
+        required: "Enter the duration of your workout."
+    },
+    distance: {
+        type: Number,
+        required: "What distance did you travel?"
+    },
+    weight: {
+        type: Number,
+        required: "How much weight did you lift?"
+    },
+    reps: {
+        type: Number,
+        required: "How many reps?"
+    },
+    sets: {
+        type: Number,
+        required: "How many sets?"
     },
     date: {
         type: Date,
